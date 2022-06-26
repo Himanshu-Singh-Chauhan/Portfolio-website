fcf3adeafe3228a7c14a30e5ced986613a062ffb
Himanshu Singh Chauhan <him.speedo@gmail.com>
2022-06-26 18:34:21 +0530

    🌈 theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/layouts/main'
-import { extendTheme } from "@chakra-ui/react"
+import theme from '../components/theme'
+import Fonts from '../components/fonts'
 
-const config = {
-  initialColorMode: "dark",
-  useSystemColorMode: false,
-}
-
-const theme = extendTheme({ config })
+// My custom styles
+// for some reason we have to imprort css files in this file only.
+import '../styles/round_scribble_underline.scss'
 
 const Website = ({ Component, pageProps, router }) => {
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
       </Layout>
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,29 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import {
+  Container,
+  Box,
+  Heading,
+  useColorModeValue,
+  Link,
+  Button
+} from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
   return (
-
     <Container>
-      
       <Box
         borderRadius="lg"
-        bg="red"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
         p={6}
         textColor="white"
         textAlign="center"
       >
-        Hello, I&apos;m 4th year CS student studing in Banglore!
+        Hello, I&apos;m final year CS student studying in Banglore!
       </Box>
-
 
       <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
@@ -24,8 +33,36 @@ const Page = () => {
         </Box>
       </Box>
 
-    </Container>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Works
+        </Heading>
+        <Paragraph>
+          {/* I'm a computer science student passionate about programming and design. pursuing my B.tech degree.
+          <br/>
+          <br/> */}
+          Here's a list of works showcasing my skills.
+        </Paragraph>
 
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My Works
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>2001</BioYear>
+          Born
+        </BioSection>
+      </Section>
+    </Container>
   )
 }
 
