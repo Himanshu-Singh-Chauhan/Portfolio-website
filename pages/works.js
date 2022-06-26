d9fc29f8fb879044c47cd6d830403f9806d25902
Himanshu Singh Chauhan <him.speedo@gmail.com>
2022-06-26 18:35:37 +0530

    👩‍🚀 added works page

---
+++ pages/works.js
@@ -0,0 +1,23 @@
+import { Container, Heading, SimpleGrid, Divider } from "@chakra-ui/react";
+import Section from '../components/section'
+import { WorkGridItem } from "../components/grid-item";
+
+const Works = () => {
+    return (
+        <Container>
+            <Heading as="h3" fontSize={20} mb={4}>
+                Works
+            </Heading>
+
+            <SimpleGrid columns={[1, 1, 2]} gap={6}>
+                <Section>
+                    <WorkGridItem id="pie_menus" title="Pie Menus" >
+                        A Circular radial menu.
+                    </WorkGridItem>
+                </Section>
+            </SimpleGrid>
+        </Container>
+    )
+}
+
+export default Works
