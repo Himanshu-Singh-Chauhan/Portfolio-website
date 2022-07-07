cb724ae8e196300948aa6373fb5265ed78dbc7cb
Himanshu Singh Chauhan <him.speedo@gmail.com>
2022-07-07 21:22:29 +0530

    🎦 animate page transition

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/layouts/main'
 import theme from '../components/theme'
 import Fonts from '../components/fonts'
+import { AnimatePresence } from 'framer-motion'
 
 // My custom styles
 // for some reason we have to imprort css files in this file only.
@@ -12,7 +13,9 @@ const Website = ({ Component, pageProps, router }) => {
     <ChakraProvider theme={theme}>
       <Fonts />
       <Layout router={router}>
+        <AnimatePresence exitBeforeEnter initial={true}>
         <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
       </Layout>
     </ChakraProvider>
   )
