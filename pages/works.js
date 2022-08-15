e32b847fb2908e009363adf8ee78ee7812dbdeeb
Himanshu Singh Chauhan <him.speedo@gmail.com>
2022-08-15 19:40:39 +0530

    updated works page

--- pages/works.js
+++ pages/works.js
@@ -2,9 +2,13 @@ import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 import Layout from '../components/layouts/article'
+import { useColorModeValue } from '@chakra-ui/react'
 
 import pie_menus_thumbnail from '../public/images/pie_menus_thumbnail.png'
-import project2 from '../public/images/pie_menus_thumbnail.png'
+import urlshortner from '../public/images/urlshortner.png'
+import portfoliowebsitelight from '../public/images/portfoliowebsitelight.png'
+import portfoliowebsitedark from '../public/images/portfoliowebsitedark.png'
+import tempthumb from '../public/images/pie_menus_thumbnail.png'
 
 const Works = () => {
   return (
@@ -14,34 +18,45 @@ const Works = () => {
           Works
         </Heading>
 
-        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <SimpleGrid columns={[1, 1, 1]} gap={6}>
           {/* project 1 */}
           <Section>
             <WorkGridItem
-              id="pie_menus"
-              title="Pie Menus"
-              thumbnail={pie_menus_thumbnail}
+              id="url_shortner"
+              title="URL Shortner"
+              thumbnail={urlshortner}
             >
-              A Circular radial menu.
+              A URL shortener service creates a short url/aliases/tiny url
+against a long url. The tiny url, then redirects to the original url.
             </WorkGridItem>
           </Section>
 
           {/* project 2 */}
           <Section>
-            <WorkGridItem id="project2" title="Project 2" thumbnail={project2}>
-              description for project 2.
+            <WorkGridItem id="portfolio" title="Portfolio Website" thumbnail={useColorModeValue(portfoliowebsitedark, portfoliowebsitelight)}>
+              Next JS + Chakra UI + Three JS
+            </WorkGridItem>
+          </Section>
+
+          {/* Project 3 */}
+
+          <Section>
+            <WorkGridItem id="pie_menus" title="Pie Menus" thumbnail={pie_menus_thumbnail}>
+            A pie menu is a menu where the items are placed in a circle around the cursor, 
+            or around a fixed position. - Compared to traditional menus, a pie menu has a shorter seeking
+            time and lower error rate.
             </WorkGridItem>
           </Section>
         </SimpleGrid>
 
-        <Section>
+        {/* <Section>
           <Divider my={6} />
           <Heading as="h3" fontSize={20}>
             Other heading style
           </Heading>
         </Section>
 
-        <SimpleGrid columns={[1, 1, 2]} gap={6}></SimpleGrid>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}></SimpleGrid> */}
       </Container>
     </Layout>
   )
